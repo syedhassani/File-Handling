ba600267927a9e8e758c39835a1a6b5bc4c6bd64
syedhassani <syedmhassani12@gmail.com>
2024-11-06 20:14:18 +1100

    File Handling

---
+++ main.py
@@ -0,0 +1,13 @@
+# # file=open("abc.txt")
+# # print(file.read())
+# # file.close()
+# file=open("abc.txt","r")
+# print(file.read())
+# read mode
+
+# write mode:
+# file=open("abc.txt","w")
+# file.write("what do you want to write")
+# append mode
+file=open("abc.txt","a")
+file.write("i want to write about sheep.sheep is cool")
